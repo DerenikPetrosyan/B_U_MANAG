85d6e4d4450922056f873b6b0ae13ef83446ea8d
Derenik Petrosyan <derenik.petrosyan@iunetworks.local>
2022-09-06 19:07:28 +0400

    Initial application setup

---
+++ um-api/src/main/java/com/iunetworks/BankUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.iunetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankUserManagementApplication.class, args);
+    }
+
+}
